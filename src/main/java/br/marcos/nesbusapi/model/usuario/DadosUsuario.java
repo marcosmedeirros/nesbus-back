1edcd0d8f3849c3fa76bbcba817a62545eff167b
Marcos Medeiros <109443751+marcosmedeirros@users.noreply.github.com>
2025-06-14 17:29:50 -0300

    Atualização do Record de exibição de DadosUsuario

--- src/main/java/br/marcos/nesbusapi/model/usuario/DadosUsuario.java
+++ src/main/java/br/marcos/nesbusapi/model/usuario/DadosUsuario.java
@@ -1,8 +1,8 @@
 package br.marcos.nesbusapi.model.usuario;
 
 
-public record DadosUsuario(Long id, String email, String nome, String cpf, String permissao) {
+public record DadosUsuario(Long id, String email, String nome, String cpf, String dataNascimento, String telefone, String permissao) {
     public DadosUsuario(Usuario usuario){
-        this(usuario.getId(), usuario.getEmail(), usuario.getNome(), usuario.getCpf(), usuario.getPermissao());
+        this(usuario.getId(), usuario.getEmail(), usuario.getNome(), usuario.getCpf(), usuario.getDataNascimento(),usuario.getTelefone(), usuario.getPermissao());
     }
 }
--- src/main/resources/db/migration/V1__create-tables-usuario-endereco.sql
+++ src/main/resources/db/migration/V1__create-tables-usuario-endereco.sql
@@ -17,7 +17,7 @@ CREATE TABLE usuario (
                          nome VARCHAR(100) NOT NULL,
                          cpf VARCHAR(14) NOT NULL UNIQUE,
                          telefone VARCHAR(15) NOT NULL UNIQUE,
-                         data_nascimento VARCHAR(10) NOT NULL,
+                         data_nascimento VARCHAR(10),
                          email VARCHAR(100) NOT NULL UNIQUE,
                          senha VARCHAR(100) NOT NULL,
                          permissao VARCHAR(20),
